9717a2c2219c919f69903993b5f780dba4cf452f
gouider heidi <gouider.heidi@gmail.com>
2023-10-20 12:24:50 +0200

    add profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -11,8 +11,10 @@ class ProfilController extends AbstractController
     #[Route('/profil', name: 'app_profil')]
     public function index(): Response
     {
+        $info = ['lastname' => 'Loper', 'firstname' => 'Dave', 'email' => 'daveloper@code.dom', 'birthdate' => '01/01/1970'];
+        
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'informations' => $info
         ]);
     }
 }
