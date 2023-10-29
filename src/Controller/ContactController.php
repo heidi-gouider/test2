d5e4d7d31ebf7f38a0b473acbedff64b2fcdb4cf
naouel89 <jessus.n@gmail.COM>
2023-10-29 16:06:39 +0100

    modif config messengeryalm

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -18,49 +18,38 @@ class ContactController extends AbstractController
     public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactFormType::class);
-        // ...
+
         // Traitement des données soumises au formulaire
         $form->handleRequest($request);
 
-        // je recupere les données transmises si le form est valide
+        // je recupere les données transmises si le form est valide et envoyé
         if ($form->isSubmitted() && $form->isValid()) {
+
             // Récupération de l'entité Contact associée au formulaire
             $contact = $form->getData();
 
             // Récupération des propriétés de l'entité Contact
             $address = $contact->getEmail();
+            // $subject = $contact->getObjet();
             // $content = $contact->getMessage();
-            $subject = $contact->getObjet();
             $content = 'objet : ' . $contact->getObjet() . "\n";
-            $content .= 'email : ' . $contact->getEmail() . "\n";
             $content .= 'message : ' . $contact->getMessage();
-
             // dd($contact);
-            // $email = $data;
-            // $adress = $data['email'];
-            // $content = $data['message'];
-            // $content = $data['objet'];
-            // $adress = $data->getEmail();
-            // $content = $data->getMessage();
-
 
             $email = (new Email())
                 ->from($address)
                 ->to('admin@admin.com')
-                ->subject($subject)
+                ->subject('demande de contact')
                 ->text($content);
 
-            // Avant l'envoi de l'email
             // dd($email);
             $mailer->send($email);
-            // Après l'envoi de l'email
-            // var_dump('Email envoyé avec succès');
-            // dd($mailer);
 
-            return $this->redirectToRoute('app_accueil');
+            // return $this->redirectToRoute('app_accueil');
         }
 
         return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
             // 'form' => $form->createView(),
             'form' => $form
         ]);
